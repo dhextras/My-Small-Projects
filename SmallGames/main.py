2d7b81d3639fa660005f3da5cce1ef816a2f0cb8
M_Urabe <104954857+UrabeM@users.noreply.github.com>
2022-06-12 17:09:22 +0530

    Update main.py

--- SmallGames/main.py
+++ SmallGames/main.py
@@ -1,71 +1,91 @@
 import you_guess
 import comp_guess
 import rock_paper_scissor
+import mine_sweeper
             
 #Choose The Game
-print("Which One You Wanna Play??")
-print("           1. For You Guess Computer's Number")
-print("           2. For Computer Guess Yours")
-print("           3. For Rock Paper Scissor Game")
-print("")
+print("""Which One You Wanna Play??
+           1. For You Guess Computer's Number
+           2. For Computer Guess Yours
+           3. For Rock Paper Scissor Game
+           4. For Mine Sweeper Game
+""")
 
 game = int(input("Choose?? "))
 print("")
 
-while game < 5:
+while game < 6:
+    main_menu = True
     if game == 1:
-        #Choose The Range
-        print("Input 2 Numbers Below Which Is Going To Be The Range For Random Number")
-        print("")
+        while main_menu == True:
+            #Choose The Range
+            print("Input 2 Numbers Below Which Is Going To Be The Range For Random Number{Only Input '0' If You wanna Go Main Menu}")
+            print("""
+* Press '0' To Go To Main Menu
+""")
 
-        first_no = int(input("Enter The 1 st Number : "))
-        second_no = int(input("Enter The 2 nd Number : "))
-        print("")
-
-        if first_no < second_no:
-            guessing = you_guess.guess(first_no, second_no)
-            print(guessing)
-        elif first_no > second_no:
-            guessing = you_guess.guess(second_no, first_no)
-            print(guessing)
-
-    elif game == 2:
-        #Choose The Range
-        print("Input 2 Numbers Below Which Is Going To Be The Range For Random Number")
-        print("")
-
-        first_no = int(input("Enter The 1 st Number : "))
-        second_no = int(input("Enter The 2 nd Number : "))
-        print("")
-
-        if first_no == second_no:
-            print(f"The Number Is {first_no}")#it can also be no_for_b bcs both same
+            first_no = int(input("Enter The 1 st Number : "))
+            second_no = int(input("Enter The 2 nd Number : "))
             print("")
 
-        else:
-            if first_no < second_no:
-                comp_guessing = comp_guess.computer_guess(first_no, second_no)
-                print(comp_guessing)
+            if first_no == 0 or second_no == 0:
+                main_menu = False
             else:
-                comp_guessing = comp_guess.computer_guess(second_no, first_no)
-                print(comp_guessing)
+                if first_no < second_no:
+                    guessing = you_guess.guess(first_no, second_no)
+                    print(guessing)
+                elif first_no > second_no:
+                    guessing = you_guess.guess(second_no, first_no)
+                    print(guessing)
+
+    elif game == 2:
+        while main_menu == True:
+            #Choose The Range
+            print("Input 2 Numbers Below Which Is Going To Be The Range For Random Number{Only Input '0' If You wanna Go Main Menu}")
+            print("""
+* Press '0' To Go To Main Menu
+""")
+
+            first_no = int(input("Enter The 1 st Number : "))
+            second_no = int(input("Enter The 2 nd Number : "))
+            print("")
+
+            if first_no == 0 or second_no == 0:
+                    main_menu = False
+            else:
+                if first_no == second_no:
+                    print(f"The Number Is {first_no}")#it can also be no_for_b bcs both same
+                    print("")
+
+                else:
+                    if first_no < second_no:
+                        comp_guessing = comp_guess.computer_guess(first_no, second_no)
+                        print(comp_guessing)
+                    else:
+                        comp_guessing = comp_guess.computer_guess(second_no, first_no)
+                        print(comp_guessing)
 
     elif game == 3:
         rps = rock_paper_scissor.Rps()
         rps.start()
 
     elif game == 4:
-        exit ()
+        ms = mine_sweeper.start()
+        print(ms)
+
+    elif game == 5:
+        exit()
 
     else:
         print("Choose The Right Game Bakayaro!!")
 
-    if game < 4:
-        print("Do You Wanna Play Again??")
-        print("           1. For You Guess Computer's Number")
-        print("           2. For Computer Guess Yours")
-        print("           3. For Rock Paper Scissor Game")
-        print("           4. If You Wanna Quit")
-        print("")
+    if game < 5:
+        print("""Do You Wanna Play Again??
+           1. For You Guess Computer's Number
+           2. For Computer Guess Yours
+           3. For Rock Paper Scissor Game
+           4. For Mine Sweeper Game
+           5. If You Wanna Quit
+""")
         game = int(input("Choose?? "))
         print("")
